c615b0324a51b0eec9f2aa63f3fc1bd0ea3d3ee0
Lauren Garcia <lauren.garcia@gmail.com>
2016-06-02 16:17:02 -0700

    rough csv rendering

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var path = require('path')
 var media = require('render-media')
 var streamToBlobURL = require('stream-to-blob-url')
+var csv = require('csv-parser')
 
 module.exports = { render: render, append: append }
 
@@ -14,18 +15,53 @@ function append (file, el, cb) {
   if (typeof el === 'string') el = document.querySelector(el)
   if (!cb) cb = function () {}
 
-  media.append(file, el, function (err, elem) {
-    if (err) {
-      elem = document.createElement('iframe')
-      streamToBlobURL(file.createReadStream(), file.length, 'text/plain', function (err, url) {
-        if (err) return cb(err)
-        elem.src = url
-        elem.sandbox = 'allow-forms allow-scripts'
-        el.appendChild(elem)
-        return cb(null, elem)
+  var extname = path.extname(file.name).toLowerCase()
+  if (extname === '.csv') {
+    var t = document.createElement('table')
+    var parser = file.createReadStream().pipe(csv())
+    parser.on('data', function (data) {
+      // if (!inc++) {
+      //   var tr = document.createElement('tr')
+      //   Object.keys(data).forEach(function (name) {
+      //     var th = document.createElement('th')
+      //     th.innerText = name
+      //     tr.appendChild(th)
+      //   })
+      //   t.appendChild(tr)
+      // }
+      var tr = document.createElement('tr')
+      Object.keys(data).forEach(function (name) {
+        var td = document.createElement('td')
+        td.innerText = data[name]
+        tr.appendChild(td)
       })
-    }
-    return cb(null, elem)
-  })
+      t.appendChild(tr)
+      el.appendChild(t)
+      // if (inc % 5 === 0) {
+      //   if (inc !== 5) $display.contentDocument.body.removeChild(more)
+      //   $display.contentDocument.body.appendChild(more)
+      //   parser.pause()
+      // }
+    })
+
+
+
+  }
+  else {
+    media.append(file, el, function (err, elem) {
+      if (err) {
+        debugger;
+        elem = document.createElement('iframe')
+        streamToBlobURL(file.createReadStream(), file.length, 'text/plain', function (err, url) {
+          if (err) return cb(err)
+          elem.src = url
+          elem.sandbox = 'allow-forms allow-scripts'
+          el.appendChild(elem)
+          return cb(null, elem)
+        })
+      }
+      return cb(null, elem)
+    })
+  }
 }
 
