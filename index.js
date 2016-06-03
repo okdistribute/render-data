9b798ecccfc0f7d27007e66e6d827c4d42f10168
Lauren Garcia <lauren.garcia@gmail.com>
2016-06-02 17:30:53 -0700

    break out lib/append-csv.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var path = require('path')
 var media = require('render-media')
 var streamToBlobURL = require('stream-to-blob-url')
-var csv = require('csv-parser')
+
+var appendCSV = require('./lib/append-csv')
 
 module.exports = { render: render, append: append }
 
@@ -17,46 +18,7 @@ function append (file, el, cb) {
 
   var extname = path.extname(file.name).toLowerCase()
   if (extname === '.csv') {
-    var t = document.createElement('table')
-    var parser = file.createReadStream().pipe(csv())
-
-    var more = document.createElement('a')
-    more.innerText = 'load 10 more'
-    more.onclick = function () {
-      parser.resume()
-    }
-    more.href = 'javascript:void(0)'
-
-    var inc = 0
-    var incrBy = 10
-    parser.on('data', function (data) {
-      if (!inc++) {
-        var tr = document.createElement('tr')
-        Object.keys(data).forEach(function (name) {
-          var th = document.createElement('th')
-          th.innerText = name
-          tr.appendChild(th)
-        })
-        t.appendChild(tr)
-      }
-      var tr = document.createElement('tr')
-      Object.keys(data).forEach(function (name) {
-        var td = document.createElement('td')
-        td.innerText = data[name]
-        tr.appendChild(td)
-      })
-      t.appendChild(tr)
-      el.appendChild(t)
-      if (inc % incrBy === 0) {
-        el.appendChild(more)
-        parser.pause()
-      }
-    })
-
-    parser.on('end', function () {
-      if (inc >= incrBy) el.removeChild(more)
-    })
-
+    appendCSV(file, el, cb)
   }
   else {
     media.append(file, el, function (err, elem) {
@@ -74,3 +36,4 @@ function append (file, el, cb) {
     })
   }
 }
+
